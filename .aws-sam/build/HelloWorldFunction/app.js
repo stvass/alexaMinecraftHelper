2949c467e233fea53d5a1a93e01f1e7bee796fe5
stvass <steve.vass@gmail.com>
2021-05-19 16:02:17 -0400

    adding files

---
+++ .aws-sam/build/HelloWorldFunction/app.js
@@ -0,0 +1,53 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+
+'use strict';
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+let data;
+let response;
+/**
+  * 
+ */
+exports.lambdaHandler = async event  => {
+  
+        //console.log("event", event.Records[0].s3 );
+
+        var params = { 
+            Bucket: 'alexadbload-mydbloadbucket-1nev4d4m336wm',
+            Key: 'minecraft.json',
+        };
+    
+        const data = await s3.getObject(params).promise();
+       
+        let minecraft = JSON.parse(data.Body.toString());
+        var itemssArray = minecraft['ITEMS'];
+        var resArray = minecraft['RECIPES'];
+       
+        resArray.forEach(function(elems) {
+
+            elems.forEach(function (elem) {
+              console.log(elem.id);
+              console.log(elem.desc);
+            })
+          })
+          
+          
+          itemssArray.forEach(function (elems) {
+              elems.forEach(function (elem) {
+                  console.log(elem.id);
+                  console.log(elem.desc);
+
+              })
+          })
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Body),
+        };
+        return response;
+    };
+   
+
+
